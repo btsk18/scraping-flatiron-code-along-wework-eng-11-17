9b1112ff7546304a442f9e2788dc4c03a9e6f4e1
Brandon Leeds <brandonleeds@gmail.com>
2017-12-26 15:28:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,12 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def get_page
+    
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +19,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
